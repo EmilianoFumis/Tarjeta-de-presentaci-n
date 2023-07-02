e4e334e07c935483b4951140c38450f25f05f570
Emiliano <emilianofumis>
2023-07-02 10:45:08 -0300

    Initial Commit

---
+++ EmiCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmiCard
+//
+//  Created by Emiliano Fumis on 02/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmiCard/EmiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmiCardApp.swift
+//  EmiCard
+//
+//  Created by Emiliano Fumis on 02/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct EmiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
