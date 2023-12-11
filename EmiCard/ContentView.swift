e0fa618f65defc0f5772dcdfd1e77b2793c0c0a2
Emiliano <emilianofumis>
2023-12-11 18:21:08 -0300

    Complete

--- EmiCard/ContentView.swift
+++ EmiCard/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    var body: some View{
+    
+    ZStack {
+        Color(UIColor(red: 0.09, green: 0.63, blue: 0.52, alpha: 1.00)).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Image("emi")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150.0, height: 150.0)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            
+            Text("Emiliano Fumis")
+                .font(Font.custom("Pacifico-Regular", size: 40))
+                .bold()
+                .foregroundColor(.white)
+            Text("Swift Developer")
+                .foregroundColor(.white)
+                .font(.system(size: 25))
+            
+            Divider()
+            InfoView(text: "+549 341 456 789", imageName: "phone.fill")
+            InfoView(text: "emilianofumis@email.com", imageName: "envelope.fill")
+            
+
+        }
+ 
+    }
+        
     }
 }
 
@@ -19,3 +46,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ EmiCard/InfoView.swift
@@ -0,0 +1,35 @@
+//
+//  InfoView.swift
+//  EmiCard
+//
+//  Created by Emiliano Fumis on 02/07/2023.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(HStack {
+                
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                
+                Text(text)
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
